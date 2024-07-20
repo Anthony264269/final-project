c3c4471a822a09db7d962283db0e36beaeb8485b
Anthony264269 <tony264269@gmail.com>
2024-07-20 10:57:05 +0200

    responsive

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -12,13 +12,14 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-    ->add('message')
+    ->add('message', TextareaType::class)
   
  
     ->add('file', FileType::class, [
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\DateTimeType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType as TypeDateTimeType;
@@ -21,7 +20,9 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('pseudo')
             ->add('country')
-            ->add('birthAt')
+            ->add('birthAt', TypeDateTimeType::class, [
+                'widget' => 'single_text',
+            ])
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
@@ -30,9 +31,9 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'label' => 'J\'accepte les conditions générales.',
             ])
             ->add('plainPassword', PasswordType::class, [
-
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -42,10 +43,18 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-
                         'max' => 4096,
                     ]),
                 ],
+            ])
+            ->add('consentRGPD', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez consentir au traitement de vos données personnelles.',
+                    ]),
+                ],
+                'label' => 'Je consens au traitement de mes données personnelles conformément à la politique de confidentialité.',
             ]);
     }
 
